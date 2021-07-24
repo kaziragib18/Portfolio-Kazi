bacd0d0a19266cd55bdf6fbcc339bd61274e693c
Kazi Md Ragib <58323531+kaziragib18@users.noreply.github.com>
2021-07-24 21:08:48 +0600

    typo fix

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
-!(function($) {
-    "use strict";
-  
-    // Preloader
-    $(window).on('load', function() {
-      if ($('#preloader').length) {
-        $('#preloader').delay(100).fadeOut('slow', function() {
-          $(this).remove();
-        });
-      }
-    });
-  
-  })(jQuery);
+!(function ($) {
+  "use strict";
+
+  // Preloader
+  $(window).on('load', function () {
+    if ($('#preloader').length) {
+      $('#preloader').delay(100).fadeOut('slow', function () {
+        $(this).remove();
+      });
+    }
+  });
+
+})(jQuery);
